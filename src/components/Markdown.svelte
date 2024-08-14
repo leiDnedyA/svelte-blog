922fda47025cc3ac249ddaa71a36437babda1f7d
ayden <aydendiel@gmail.com>
2024-08-14 14:48:18 -0400

    fix: updated md renderer to use base path

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -2,11 +2,12 @@
   export let src: string;
 	import { marked } from 'marked';
 	import { onMount } from 'svelte';
+  import { base } from '$app/paths';
 
 	let html = '';
 
 	onMount(async () => {
-		const response = await fetch(src);
+		const response = await fetch(base + src);
 		const md = await response.text();
 		html = await marked.parse(md);
 	});
